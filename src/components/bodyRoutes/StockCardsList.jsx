aaa6c10a1102588ef33f687ae3adbf453a7e505c
Satnam1313 <satnamsingh13135@gmail.com>
2017-03-04 08:27:57 -0500

    improved UI of StockCardList

--- src/components/bodyRoutes/StockCardsList.jsx
+++ src/components/bodyRoutes/StockCardsList.jsx
@@ -17,26 +17,57 @@ class StockCards extends React.Component{
 	render(){
 		console.log(this.props)
 		return(                                    	
-		<div className="col-sm-3">
-			<div className="card">
-					<div className="card-block">
-					<h3 className="card-title">{this.props.content.symbol}</h3>
-					<ul className="list-group">
-						<li className="list-group-item">Last bid = 85.58</li>
-						<li className="list-group-item">change = +9.2</li>
-						<li className="list-group-item">% change = 25%</li>
-					</ul>
-					<ul>
-						<li className="list-group-item">
-							<a className="card-text btn btn-sm"><i className="fa fa-bars"></i> Details</a>	
-						</li>
-						<li className="list-group-item">
-							<a onClick={this.showOverlay.bind(this)}className="card-text btn btn-sm"><i className="fa fa-bars" ></i> Buy/Sell</a>	
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
+		<div class="container">
+
+       <section class="col-sm-12">
+           <div class="col-sm-12 col-md-6">
+               Part for Graph
+           </div>
+           <div class="col-sm-12 col-md-6 " style="border: black ridge .1em;border-radius: 15px 50px; padding: 20px;
+     padding-top: 10px">
+               <div >
+                   <pre class="col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 ">Current price</pre><pre class="col-sm-4 col-sm-offset-1 col-md-4 col-md-offset-1">Symbol</pre>     <pre class="col-sm-4 col-sm-offset-1 col-md-4 col-md-offset-2">Last </pre>
+                   <pre class="col-sm-4 col-sm-offset-1 col-md-4 col-md-offset-1">Change </pre><pre class="col-sm-4 col-sm-offset-1 col-md-4 col-md-offset-2"> %Change</pre>
+               </div>
+           </div>
+       </section>
+           </div>
+<div class="container-fluid" style="padding: 2%">
+
+    <section class="col-sm-10 col-sm-offset-1 ">
+           <div  class="row">
+
+           <div class="form-horizontal">
+               <div class="form-group">
+               <label class="col-xs-1 text-center">Symbol </label>
+                   <label class="col-xs-2  text-center">Last Quantity </label>
+                   <label class="col-xs-1 text-center">Bid Size </label>
+                   <label class="col-xs-1 text-center">Offer Price</label>
+                   <label class="col-xs-1 text-center">Offer Size</label>
+                   <label class="col-xs-2 text-center">Previous Close Price</label>
+                   <label class="col-xs-1 text-centertext-center">High Price </label>
+                   <label class="col-xs-1 text-center">Low Price </label>
+                   <label class="col-xs-1 text-center">Trade Volume </label>
+               </div>
+               <hr>
+           </div>
+               <div class="form-horizontal">
+                   <div class="form-group">
+                       <label class="col-xs-1 text-center">RBC </label>
+                       <label class="col-xs-2  text-center">123</label>
+                       <label class="col-xs-1 text-center">12.132 </label>
+                       <label class="col-xs-1 text-center">12.36</label>
+                       <label class="col-xs-1 text-center">10</label>
+                       <label class="col-xs-2 text-center">11.1023</label>
+                       <label class="col-xs-1 text-centertext-cent0er">12.36</label>
+                       <label class="col-xs-1 text-center">11.1023</label>
+                       <label class="col-xs-1 text-center">12013</label>
+                   </div>
+                   <hr>
+               </div>
+           </div>
+    </section>
+</div>
 
 		)
 	}
